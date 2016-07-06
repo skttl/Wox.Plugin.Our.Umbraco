8ec29600ee164f49a83219145e8ea5d20f233ce7
skttl <sk@ecreo.dk>
2016-07-06 21:12:34 +0200

    fix dates, add search for specific type

--- Wox.Plugin.Our.Umbraco/Our.cs
+++ Wox.Plugin.Our.Umbraco/Our.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
 
@@ -19,10 +20,8 @@ namespace Wox.Plugin.Our.Umbraco
         {
         }
 
-        /*public string RelativeDate(DateTime yourDate)
+        public string RelativeDate(DateTime yourDate)
         {
-            return "today";
-            /*
             const int SECOND = 1;
             const int MINUTE = 60 * SECOND;
             const int HOUR = 60 * MINUTE;
@@ -75,13 +74,13 @@ namespace Wox.Plugin.Our.Umbraco
                 int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
                 return years <= 1 ? "one year ago" : years + " years ago";
             }
-        }*/
+        }
 
         public DateTime StringToDate(string date)
         {
             try
             {
-                return DateTime.ParseExact(date, "yyyyMMddHHmmss", System.Globalization.CultureInfo.InvariantCulture);
+                return DateTime.ParseExact(date, "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture);
             }
             catch (Exception e)
             {
@@ -89,31 +88,61 @@ namespace Wox.Plugin.Our.Umbraco
             }
         }
 
+        public string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+
+        public string Truncate(string source, int length)
+        {
+            if (source.Length > length)
+            {
+                source = source.Substring(0, length) + "...";
+            }
+            var result = Regex.Replace(source, @"\r\n?|\n", " ");
+            return result;
+        }
+
         public string GenerateSubTitle(JToken fields)
         {
             try
             {
+                var subtitle = "";
                 switch (fields["nodeTypeAlias"].ToString())
                 {
                     case "forum":
-                        var subtitle = "Posted " + StringToDate(fields["createDate"].ToString()) + " by: " +
+                        subtitle = "Posted " + RelativeDate(StringToDate(fields["createDate"].ToString())) + " by: " +
                                        fields["authorName"];
 
                         if (fields["replies"].ToString() != "0")
                         {
                             subtitle = subtitle + " - " + fields["replies"] + " replies, last reply " +
-                                       StringToDate(fields["updateDate"].ToString()) + " by " +
-                                       fields["lastReplyAuthorName"];
+                                       RelativeDate(StringToDate(fields["createDate"].ToString())) + " by " +
+                                       fields["latestReplyAuthorName"];
                         }
                         else
                         {
                             subtitle = subtitle + " - No replies";
                         }
-
-                        return subtitle;
+                        subtitle = subtitle + Environment.NewLine + Truncate(fields["body"].ToString(), 140);
+                        break;
+                    case "project":
+                        subtitle = FirstLetterToUpper(fields["categoryFolder"].ToString()) + " - ❤ " + fields["karma"] + " - Downloads " + fields["downloads"] + " - Compatible versions: " + fields["compatVersions"] + Environment.NewLine + Truncate(fields["body"].ToString(), 140);
+                        break;
+                    case "documentation":
+                        subtitle = Truncate(fields["body"].ToString(), 140);
+                        break;
                     default:
-                        return "";
+                        subtitle = fields["url"].ToString();
+                        break;
                 }
+                return subtitle;
             }
             catch (ExternalException e)
             {
@@ -126,7 +155,11 @@ namespace Wox.Plugin.Our.Umbraco
             switch (fields["nodeTypeAlias"].ToString())
             {
                 case "forum":
-                    return "our.forum.png";
+                    return "our.forum" + (fields["replies"].ToString() != "0" ? ".replies" : "") + ".png";
+                case "project":
+                    return "our.package.png";
+                case "documentation":
+                    return "our.docs.png";
                 default:
                     return "our.png";
             }
@@ -136,24 +169,80 @@ namespace Wox.Plugin.Our.Umbraco
         {
 
             WebClient client = new WebClient();
-            var rawResponse = client.DownloadString("https://our.umbraco.org/umbraco/api/OurSearch/GetGlobalSearchResults/?term=" + query.Search);
-            JObject response = JObject.Parse(rawResponse);
-            JArray items = (JArray)response["items"];
 
-            var result = new List<Result>();
+            var search = query.Search;
+            var method = "GetGlobalSearchResults";
+            var fullSearchPostfix = "";
+            var fullSearchTitle = "";
 
-            foreach (var item in items.Take(5))
+            if (query.FirstSearch == "/p")
             {
+                method = "GetProjectSearchResults";
+                fullSearchPostfix = "&cat=project";
+                fullSearchTitle = "projects";
+                search = query.SecondToEndSearch;
+            }
+
+            if (query.FirstSearch == "/d")
+            {
+                method = "GetDocsSearchResults";
+                fullSearchPostfix = "&cat=documentation";
+                fullSearchTitle = "documentation";
+                search = query.SecondToEndSearch;
+            }
+
+            if (query.FirstSearch == "/f")
+            {
+                method = "GetForumSearchResults";
+                fullSearchPostfix = "&cat=forum";
+                fullSearchTitle = "forum";
+                search = query.SecondToEndSearch;
+            }
+
+            if (search != "/" && search != "")
+            {
+                var rawResponse =
+                    client.DownloadString("https://our.umbraco.org/umbraco/api/OurSearch/" + method + "/?term=" + search);
+                JObject response = JObject.Parse(rawResponse);
+                JArray items = (JArray) response["items"];
+
+                var result = new List<Result>();
+
+                foreach (var item in items.Take(5))
+                {
+                    result.Add(new Result()
+                    {
+                        Title = item["Fields"]["nodeName"].ToString(),
+                        SubTitle = GenerateSubTitle(item["Fields"]),
+                        IcoPath = GetIcon(item["Fields"]),
+                        Action = c =>
+                        {
+                            try
+                            {
+                                Process.Start("https://our.umbraco.org" + item["Fields"]["url"].ToString());
+                                return true;
+                            }
+                            catch (ExternalException e)
+                            {
+                                MessageBox.Show("Open failed, please try later");
+                                return false;
+                            }
+                        }
+                    });
+                }
+
                 result.Add(new Result()
                 {
-                    Title = item["Fields"]["nodeName"].ToString(),
-                    SubTitle = GenerateSubTitle(item["Fields"]),
-                    IcoPath = GetIcon(item["Fields"]),
+                    Title =
+                        "Search " + fullSearchTitle + (fullSearchTitle != "" ? " on " : "") + "our.umbraco.org for '" +
+                        search + "'",
+                    SubTitle = items.Count + " result" + (items.Count != 1 ? "s" : ""),
+                    IcoPath = "our.png",
                     Action = c =>
                     {
                         try
                         {
-                            Process.Start(item["Fields"]["url"].ToString());
+                            Process.Start("https://our.umbraco.org/search?q=" + search + fullSearchPostfix);
                             return true;
                         }
                         catch (ExternalException e)
@@ -163,9 +252,13 @@ namespace Wox.Plugin.Our.Umbraco
                         }
                     }
                 });
-            }
 
-            return result;
+                return result;
+            }
+            else
+            {
+                return new List<Result>();
+            }
         }
     }
 }
